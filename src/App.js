94f76f5d68baedb7b4e95a5cdfe6e2462701d610
Trisha <trisha@Admins-MacBook-Pro.local>
2020-10-29 11:33:28 +0530

    added cart-icon and cart-dropdown to our application, and also implemented card-reducer

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user.actions';
+import { setCurrentUser } from './redux/user/user.actions';
 
 
 class App extends React.Component {
