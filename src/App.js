343203fffd09cf8856a0305c560e61485da61796
Trisha <trisha@Admins-MacBook-Pro.local>
2020-11-22 10:26:44 +0530

    implement the code that pulls in our shop data from firestore and stores it in shop reducer from our shop page. Also included our withSpinner HOC

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    const {setCurrentUser}= this.props
+    const {setCurrentUser }= this.props; 
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
@@ -72,7 +72,7 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
